291f39c2407b9ce1aca56fc6acc220e883538d00
Julio Condor <julioc3bgud@gmail.com>
2023-06-18 01:08:56 -0500

    Examen al 98% finiquiando animaciones...

--- js/hamburger.js
+++ js/hamburger.js
@@ -11,17 +11,3 @@ window.onscroll = () => {
   navbar.classList.remove("open-menu");
   menu.classList.remove("move");
 };
-
-// Carousel Swiper
-let swiper = new Swiper(".reviews-content", {
-  spaceBetween: 30,
-  centeredSlides: true,
-  autoplay: {
-    delay: 4000,
-    disableOnInteraction: true,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-});
